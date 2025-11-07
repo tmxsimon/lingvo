681de21d05300470eefe88eaa5600f7581480047
timxsimon <tm.simon@proton.me>
2025-11-07 21:22:12 +0100

    feat: add frontend and backend

---
+++ frontend/src/App.tsx
@@ -0,0 +1,9 @@
+const App = () => {
+  return (
+    <main className="h-screen flex justify-center items-center">
+      <h1 className="text-9xl text-center text-blue-500">Lingvo</h1>
+    </main>
+  );
+};
+
+export default App;
