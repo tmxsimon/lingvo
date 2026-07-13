82835f2bc196afdb3296fdea4d6f2b937c5a01d3
tmxsimon <tm.simon@proton.me>
2026-07-13 13:22:20 +0200

    fix: text color in light theme

--- frontend/src/features/cards/hooks/useCardEntry.ts
+++ frontend/src/features/cards/hooks/useCardEntry.ts
@@ -1,4 +1,4 @@
-import { useCallback, useEffect, useMemo, useRef, useState } from "react";
+import { useCallback, useEffect, useMemo, useState } from "react";
 import type { DictionaryEntryType } from "../../dictionary/types";
 import { fetchCardsEntries } from "../services";
 import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
